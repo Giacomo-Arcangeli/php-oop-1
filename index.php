cd38f66f016f799d41d7bd08eb3ca296eeb79011
Giacomo-Arcangeli <giacomoarcangeli1997@gmail.com>
2023-07-24 19:06:31 +0200

    aggiunto metodo e stampa in pagina

--- index.php
+++ index.php
@@ -12,13 +12,35 @@ public function __construct($titolo, $durata, $genere, $anno){
     $this->genere = $genere;
     $this->anno = $anno;
 }
-
+public function getTitle(){
+    return $this-> titolo;
+}
 }
 
+
 $movie1= new Movie('Titanic', '3h', 'Drammatico',1997 );
-$movie2 = new Movie('Avatar', '2,45h','Fantascienza',2009);
+$movie2 = new Movie('Avatar', '2.45h' ,'Fantascienza',2009);
 
 var_dump($movie1);
 var_dump($movie2);
+echo 'Titolo film:' . ' ' . $movie1->getTitle();
+
+$movies = [$movie1, $movie2];
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <ul>
+        <?php foreach ($movies as $movie) :?>
+        <li><?=$movie->titolo.' '.$movie->durata.' '.$movie->genere.' '.$movie->anno?></li>
+        <?php endforeach ?>
+    </ul>
+</body>
+</html>
